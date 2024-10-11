0c775198316ceb8aff1a993540f045fc40771113
odxscholars <shemgithub@protonmail.com>
2024-10-11 19:07:37 +0800

    added files and changed main to disable automatic logging

--- Main.cpp
+++ Main.cpp
@@ -1,79 +1,69 @@
 #include <iostream>
-#include <string>
-#include <cstdlib> 
-
-
-void headerDisplay() {
-    std::cout << "          _____                    _____                   _______                   _____                    _____                    _____                _____          \n";
-    std::cout << "         /\\    \\                  /\\    \\                 /::\\    \\                 /\\    \\                  /\\    \\                  /\\    \\              |\\    \\         \n";
-    std::cout << "        /::\\    \\                /::\\    \\               /::::\\    \\               /::\\    \\                /::\\    \\                /::\\    \\             |:\\____\\        \n";
-    std::cout << "       /::::\\    \\              /::::\\    \\             /::::::\\    \\             /::::\\    \\              /::::\\    \\              /::::\\    \\            |::|   |        \n";
-    std::cout << "      /::::::\\    \\            /::::::\\    \\           /::::::::\\    \\           /::::::\\    \\            /::::::\\    \\            /::::::\\    \\           |::|   |        \n";
-    std::cout << "     /:::/\\:::\\    \\          /:::/\\:::\\    \\         /:::/~~\\:::\\    \\         /:::/\\:::\\    \\          /:::/\\:::\\    \\          /:::/\\:::\\    \\          |::|   |        \n";
-    std::cout << "    /:::/  \\:::\\    \\        /:::/__\\:::\\    \\       /:::/    \\:::\\    \\       /:::/__\\:::\\    \\        /:::/__\\:::\\    \\        /:::/__\\:::\\    \\         |::|   |        \n";
-    std::cout << "   /:::/    \\:::\\    \\       \\:::\\   \\:::\\    \\     /:::/    / \\:::\\    \\     /::::\\   \\:::\\    \\      /::::\\   \\:::\\    \\       \\:::\\   \\:::\\    \\        |::|   |        \n";
-    std::cout << "  /:::/    / \\:::\\    \\    ___\\:::\\   \\:::\\    \\   /:::/____/   \\:::\\____\\   /::::::\\   \\:::\\    \\    /::::::\\   \\:::\\    \\    ___\\:::\\   \\:::\\    \\       |::|___|______  \n";
-    std::cout << " /:::/    /   \\:::\\    \\  /\\   \\:::\\   \\:::\\    \\ |:::|    |     |:::|    | /:::/\\:::\\   \\:::\\____\\  /:::/\\:::\\   \\:::\\    \\  /\\   \\:::\\   \\:::\\    \\      /::::::::\\    \\ \n";
-    std::cout << "/:::/____/     \\:::\\____\\/::\\   \\:::\\   \\:::\\____\\|:::|____|     |:::|    |/:::/  \\:::\\   \\:::|    |/:::/__\\:::\\   \\:::\\____\\/::\\   \\:::\\   \\:::\\____\\    /::::::::::\\____\\\n";
-    std::cout << "\\:::\\    \\      \\::/    /\\:::\\   \\:::\\   \\::/    / \\:::\\    \\   /:::/    / \\::/    \\:::\\  /:::|____|\\:::\\   \\:::\\   \\::/    /\\:::\\   \\:::\\   \\::/    /   /:::/~~~~/~~      \n";
-    std::cout << " \\:::\\    \\      \\/____/  \\:::\\   \\:::\\   \\/____/   \\:::\\    \\ /:::/    /   \\/_____\\/:::\\/:::/    /  \\:::\\   \\:::\\   \\/____/  \\:::\\   \\:::\\   \\/____/   /:::/    /         \n";
-    std::cout << "  \\:::\\    \\               \\:::\\   \\:::\\    \\        \\:::\\    /:::/    /             \\::::::/    /    \\:::\\   \\:::\\    \\       \\:::\\   \\:::\\    \\      /:::/    /          \n";
-    std::cout << "   \\:::\\    \\               \\:::\\   \\:::\\____\\        \\:::\\__/:::/    /               \\::::/    /      \\:::\\   \\:::\\____\\       \\:::\\   \\:::\\____\\    /:::/    /           \n";
-    std::cout << "    \\:::\\    \\               \\:::\\  /:::/    /         \\::::::::/    /                 \\::/____/        \\:::\\   \\::/    /        \\:::\\  /:::/    /    \\::/    /            \n";
-    std::cout << "     \\:::\\    \\               \\:::\\/:::/    /           \\::::::/    /                   ~~               \\:::\\   \\/____/          \\:::\\/:::/    /      \\/____/             \n";
-    std::cout << "      \\:::\\    \\               \\::::::/    /             \\::::/    /                                      \\:::\\    \\               \\::::::/    /                            \n";
-    std::cout << "       \\:::\\____\\               \\::::/    /               \\::/____/                                        \\:::\\____\\               \\::::/    /                             \n";
-    std::cout << "        \\::/    /                \\::/    /                 ~~                                               \\::/    /                \\::/    /                              \n";
-    std::cout << "         \\/____/                  \\/____/                                                                    \\/____/                  \\/____/                               \n";
-    std::cout << "____________________________________________________________________________________________________________\n\n";
-    std::cout << "                              Command Line Emulator\n";
-    std::cout << "____________________________________________________________________________________________________________\n\n";
-
-    // Display the list of available commands
-    std::cout << "Available Commands:\n";
-    std::cout << "  initialize     - Initialize something\n";
-    std::cout << "  screen         - Manage screen operations\n";
-    std::cout << "  scheduler-test - Test the scheduler\n";
-    std::cout << "  scheduler-stop - Stop the scheduler\n";
-    std::cout << "  report-util    - Run report utility\n";
-    std::cout << "  clear          - Clear the screen\n";
-    std::cout << "  exit           - Exit the application\n";
-    std::cout << "____________________________________________________________________________________________________________\n\n";
-}
-
-
-void clearScreen() {
-#ifdef _WIN32
-    system("cls"); 
-#endif
-    headerDisplay();
-}
+#include "Scheduler.h"
 
 int main() {
+    const int numCores = 4;
+    const int numProcesses = 10;
+    const int numPrints = 100;
+    const std::string printString = "This is a print command";
+
+    Scheduler scheduler(numCores);
+
+
+
+    //make 10 processes
+
+
+    // auto process0 = std::make_shared<Process>(0, 32, "Print command for process 0");
+    // auto process1 = std::make_shared<Process>(1, 80, "Print command for process 1");
+    // auto process2 = std::make_shared<Process>(2, 96, "Print command for process 2");
+    // auto process3 = std::make_shared<Process>(3, 100, "Print command for process 3");
+    // auto process4 = std::make_shared<Process>(4, 100, "Print command for process 4");
+    // auto process5 = std::make_shared<Process>(5, 60, "Print command for process 5");
+    // auto process6 = std::make_shared<Process>(6, 100, "Print command for process 6");
+    // auto process7 = std::make_shared<Process>(7, 100, "Print command for process 7");
+    // auto process8 = std::make_shared<Process>(8, 100, "Print command for process 8");
+    // auto process9 = std::make_shared<Process>(9, 100, "Print command for process 9");
+    //
+    // // Add each process to the scheduler
+    // scheduler.addProcess(process0);
+    // scheduler.addProcess(process1);
+    // scheduler.addProcess(process2);
+    // scheduler.addProcess(process3);
+    // scheduler.addProcess(process4);
+    // scheduler.addProcess(process5);
+    // scheduler.addProcess(process6);
+    // scheduler.addProcess(process7);
+    // scheduler.addProcess(process8);
+    // scheduler.addProcess(process9);
+    for (int i = 0; i < numProcesses; ++i) {
+        auto process = std::make_shared<Process>(i, numPrints, printString);
+        scheduler.addProcess(process);
+    }
+
+
+    scheduler.start();
+
+
+
+    // UNCOMMENT IF you want a consistent status update
+
+    // std::thread statusThread([&scheduler]() {
+    //     while (true) {
+    //         std::this_thread::sleep_for(std::chrono::seconds(1));
+    //
+    //         scheduler.displayStatus();
+    //     }
+    // });
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "Type 'exit' to close the emulator or 'screen -ls' to show running processes.\n";
     std::string command;
-    headerDisplay();
-
-    while (true) {
-        std::cout << "Enter command: ";
-        std::getline(std::cin, command);
-
-        if (command == "initialize") {
-            std::cout << "initialize command recognized. Doing something.\n";
-        } else if (command == "screen") {
-            std::cout << "screen command recognized. Doing something.\n";
-        } else if (command == "scheduler-test") {
-            std::cout << "scheduler-test command recognized. Doing something.\n";
-        } else if (command == "scheduler-stop") {
-            std::cout << "scheduler-stop command recognized. Doing something.\n";
-        } else if (command == "report-util") {
-            std::cout << "report-util command recognized. Doing something.\n";
-        } else if (command == "clear") {
-            clearScreen();
-        } else if (command == "exit") {
-            std::cout << "Exiting the application.\n";
+    while (std::getline(std::cin, command)) {
+        if (command == "exit") {
             break;
-        } else {
-            std::cout << "Unknown command. Try again.\n";
+        } else if (command == "screen -ls") {
+            scheduler.displayStatus();
         }
     }
 
