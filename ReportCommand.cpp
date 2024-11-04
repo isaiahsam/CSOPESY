ffdc47e098e584561c743947e6fa779daf4a2c31
Ethea2 <nathandres2001@gmail.com>
2024-11-04 17:07:06 +0800

    added: base code for report-util

--- ReportCommand.cpp
+++ ReportCommand.cpp
@@ -1,7 +1,18 @@
 #include "ReportCommand.h"
 #include <iostream>
+#include <fstream>
+#include <filesystem> 
 
 void ReportCommand::runReportUtil() {
-    std::cout << "Running report utility...\n";
     // Logic for report generation
+    std::filesystem::path current_dir = std::filesystem::current_path();
+    std::ofstream outputFile("csopesy-log.txt");
+    std::cout << "Running report utility...\n";
+
+    if(outputFile.is_open()) {
+        outputFile << "Hello reports!";
+        std::cout << "Report successfully written to " << current_dir / "csopesy-log.txt" << std::endl;
+    } else {
+        std::cout << "Something went wrong!";
+    }
 }
