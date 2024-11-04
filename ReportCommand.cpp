9e6cc65d7eac1b63a60611677a78a459af8fe05f
isaiahsam <isaiahpascual12@gmail.com>
2024-11-04 22:37:12 +0800

    draft reportcommand.cpp

--- ReportCommand.cpp
+++ ReportCommand.cpp
@@ -1,18 +1,40 @@
 // #include "ReportCommand.h"
+// #include "Scheduler.h"
 // #include <iostream>
 // #include <fstream>
-// #include <filesystem> 
+// #include <filesystem>
+
+// extern Scheduler scheduler;
 
 // void ReportCommand::runReportUtil() {
-//     // Logic for report generation
+
 //     std::filesystem::path current_dir = std::filesystem::current_path();
 //     std::ofstream outputFile("csopesy-log.txt");
 //     std::cout << "Running report utility...\n";
 
-//     if(outputFile.is_open()) {
-//         outputFile << "Hello reports!";
+//     if (outputFile.is_open()) {
+
+//         int coresUsed = scheduler.getCoresUsed();
+//         int totalCores = scheduler.getNumCores();
+//         double cpuUtilization = scheduler.getCpuUtilization();
+//         const std::vector<Process>& allProcesses = scheduler.getAllProcesses();
+
+//         outputFile << "=== Screen -ls Report ===\n";
+//         outputFile << "Total Cores: " << totalCores << "\n";
+//         outputFile << "Cores in Use: " << coresUsed << "\n";
+//         outputFile << "CPU Utilization: " << cpuUtilization << "%\n\n";
+//         outputFile << "Processes:\n";
+
+//         for (const auto& process : allProcesses) {
+//             outputFile << "Process Name: " << process.getName() << "\n";
+//             outputFile << "Instruction Count: " << process.getInstructionCount() << "\n";
+//             outputFile << "Instructions Executed: " << process.getInstructionsExecuted() << "\n";
+//             outputFile << "Core Assigned: " << process.getCoreAssigned() << "\n";
+//             outputFile << "---------------------------\n";
+//         }
+
 //         std::cout << "Report successfully written to " << current_dir / "csopesy-log.txt" << std::endl;
 //     } else {
-//         std::cout << "Something went wrong!";
+//         std::cout << "Something went wrong while opening the file!" << std::endl;
 //     }
 // }
